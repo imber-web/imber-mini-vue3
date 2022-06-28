30fdefca7677b97af548a8277a03b4659c3c1f4f
imber <a1157911285@163.com>
2022-06-28 22:01:45 +0800

    feat:effect函数

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,1 +1,14 @@
-export function effect() {}
+export class ReactiveEffect {
+  public active = true
+  // 传一个fn，就直接放到this上
+  constructor(public fn) {}
+  run() {
+    this.fn() //去proxy对象上取值
+  }
+}
+
+export function effect(fn) {
+  //将传递的函数变成响应式的effect
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
